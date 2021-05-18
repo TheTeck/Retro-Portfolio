5173867d903724048b5706699e143217502c9484
John Teckemeyer <johnteckemeyer@gmail.com>
2021-05-18 00:23:43 -0700

    Finalize landing page and play with portal to rest of the site

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -6,6 +6,7 @@ export default function Landing () {
     return (
         <div id="landing-container">
             <h1 id="title-header">JOHN TECKEMEYER</h1>
+            <h1 id="title-header-glow">JOHN TECKEMEYER</h1>
             <h2 id="sub-title-header">Web Developer</h2>
             <div id="scroll-indicator">
                 <Chevron dir={'down'} size={30} />
--- src/Components/Space/Space.jsx
+++ src/Components/Space/Space.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cube from '../../Components/Cube/Cube'
 import Panel from '../../Components/Panel/Panel'
 import Arc from '../../Components/Arc/Arc'
@@ -6,13 +6,29 @@ import './Space.css'
 
 export default function Space() {
 
+    const [perspective, setPerspective] = useState(100)
     
+    function movePerspective(e) {
+        if (perspective < 200 && e.clientY < window.innerHeight / 2) {
+            const temp = perspective
+            setPerspective(temp + 2)
+        }
+        if (perspective > 0 && e.clientY > window.innerHeight / 2) {
+            const temp = perspective
+            setPerspective(temp - 2)
+        }
+    }
 
     return (
-        <div className="space-container">
+        <div 
+            className="space-container" 
+            style={{ perspectiveOrigin: `50% calc(50% - ${perspective}px)` }}
+            onMouseMove={movePerspective}
+            >
             <Cube xLoc={0} yLoc={0} zLoc={0} size={200} />
             <Arc />
             <Panel />
+            <div id="enter-text">ENTER</div>
         </div>
     )
 }
