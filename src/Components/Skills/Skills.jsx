6ecfda34c0076c08f780d38da321a2458c101f82
John Teckemeyer <johnteckemeyer@gmail.com>
2021-05-28 09:12:48 -0700

    Make updates to CSS for better user experience

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -21,21 +21,21 @@ export default function Skills () {
                     <div className="skill" onMouseOver={showSkill}>CSS</div>
                     <div className="skill" onMouseOver={showSkill}>GIT</div>
                     <div className="skill" onMouseOver={showSkill}>GITHUB</div>
-                    <div className="skill" onMouseOver={showSkill}>AJAX</div>
                     <div className="skill" onMouseOver={showSkill}>REACT</div>
                     <div className="skill" onMouseOver={showSkill}>REACT ROUTER</div>
+                    <div className="skill" onMouseOver={showSkill}>AJAX</div>
                     <div className="skill" onMouseOver={showSkill}>NODE.JS</div>
-                    <div className="skill" onMouseOver={showSkill}>EXPRESS.JS</div>
+                    <div className="skill" onMouseOver={showSkill}>EXPRESSJS</div>
                     <div className="skill" onMouseOver={showSkill}>DJANGO</div>
                     <div className="skill" onMouseOver={showSkill}>MONGODB</div>
                     <div className="skill" onMouseOver={showSkill}>POSTGRESQL</div>
-                    <div className="skill" onMouseOver={showSkill}>MONGOOSE.JS</div>
+                    <div className="skill" onMouseOver={showSkill}>MONGOOSE</div>
                     <div className="skill" onMouseOver={showSkill}>SEMANTIC UI</div>
-                    <div className="skill" onMouseOver={showSkill}>MATERIALIZE</div>
                     <div className="skill" onMouseOver={showSkill}>EJS</div>
-                    <div className="skill" onMouseOver={showSkill}>JSX</div>
+                    <div className="skill" onMouseOver={showSkill}>MATERIALIZE</div>
                     <div className="skill" onMouseOver={showSkill}>OAUTH</div>
-                    <div className="skill" onMouseOver={showSkill}>PASSPORT.JS</div>
+                    <div className="skill" onMouseOver={showSkill}>PASSPORTJS</div>
+                    <div className="skill" onMouseOver={showSkill}>JSX</div>
                     <div className="skill" onMouseOver={showSkill}>JWT</div>
                     <div className="skill" onMouseOver={showSkill}>AWS</div>
                     <div className="skill" onMouseOver={showSkill}>ATLAS</div>
