cdf951d86484182c235783278db801b49bd0deb7
John Teckemeyer <johnteckemeyer@gmail.com>
2021-06-01 21:52:48 -0700

    Add all the sounds for the piano

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,9 +5,37 @@ import './About.css'
 export default function About () {
 
     const c1 = new Audio('C1.mp3')
+    const d1 = new Audio('D1.mp3')
+    const eb1 = new Audio('Eb1.mp3')
+    const f1 = new Audio('F1.mp3')
+    const g1 = new Audio('G1.mp3')
+    const ab1 = new Audio('Ab1.mp3')
+    const bb1 = new Audio('Bb1.mp3')
+    const c2 = new Audio('C2.mp3')
 
     function handleC1Key () {
         c1.play()
+    }
+    function handleD1Key () {
+        d1.play()
+    }
+    function handleEb1Key () {
+        eb1.play()
+    }
+    function handleF1Key () {
+        f1.play()
+    }
+    function handleG1Key () {
+        g1.play()
+    }
+    function handleAb1Key () {
+        ab1.play()
+    }
+    function handleBb1Key () {
+        bb1.play()
+    }
+    function handleC2Key () {
+        c2.play()
     }
 
     return (
@@ -34,13 +62,13 @@ export default function About () {
             <div id="city-floor">
                 <div className="floor-flare"></div>
                 <div onClick={handleC1Key} className="floor-col"></div>
-                <div className="floor-col"></div>
-                <div className="floor-col"></div>
-                <div className="floor-col"></div>
-                <div className="floor-col"></div>
-                <div className="floor-col"></div>
-                <div className="floor-col"></div>
-                <div className="floor-col"></div>
+                <div onClick={handleD1Key} className="floor-col"></div>
+                <div onClick={handleEb1Key} className="floor-col"></div>
+                <div onClick={handleF1Key} className="floor-col"></div>
+                <div onClick={handleG1Key} className="floor-col"></div>
+                <div onClick={handleAb1Key} className="floor-col"></div>
+                <div onClick={handleBb1Key} className="floor-col"></div>
+                <div onClick={handleC2Key} className="floor-col"></div>
             </div>
             <div id="about-title">About Me</div>
             {/* <div id="about-info">I am a full-stack web developer out of the San Francisco bay area - determined to bring unique experiences for users through out-of-the-box solutions. With a background in music composition and luthiery, I can't help but to strive to implement as much artistry in my projects as I can.</div> */}
